1e8b757e52e0464bea9866d42904e8ae1a1cbe46
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 01:40:10 +0530

    First Commit

---
+++ src/components/RegistrationForm.tsx
@@ -0,0 +1,60 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+interface RegistrationFormProps {
+  onRegister: (name: string, email: string, password: string) => void;
+}
+
+const RegistrationForm: React.FC<RegistrationFormProps> = ({ onRegister }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    console.log('Registering user:', name, email, password);
+    onRegister(name, email, password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Register" onPress={handleRegister} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
+
+export default RegistrationForm;
